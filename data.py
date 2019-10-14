43e87d4cba6b30595d679a100ae6390152c34a40
subro608 <46515291+subro608@users.noreply.github.com>
2019-10-14 19:57:22 +0530

    #1_initial_commit

---
+++ data.py
@@ -0,0 +1,160 @@
+import pandas as pd
+import numpy as np
+from tqdm import tqdm, trange
+from keras.preprocessing.sequence import pad_sequences
+import torch
+from torch.optim import Adam
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from pytorch_pretrained_bert import BertTokenizer, BertConfig
+from pytorch_pretrained_bert import BertForTokenClassification, BertAdam
+from sklearn.model_selection import train_test_split
+from seqeval.metrics import f1_score
+
+
+class SentenceGetter(object):
+
+    def __init__(self, data):
+        self.n_sent = 1
+        self.data = data
+        self.empty = False
+        agg_func = lambda s: [(w, p, t) for w, p, t in zip(s["Word"].values.tolist(),
+                                                           s["POS"].values.tolist(),
+                                                           s["Tag"].values.tolist())]
+        self.grouped = self.data.groupby("Sentence #").apply(agg_func)
+        self.sentences = [s for s in self.grouped]
+
+    def get_next(self):
+        try:
+            s = self.grouped["Sentence: {}".format(self.n_sent)]
+            self.n_sent += 1
+            return s
+        except:
+            return None
+
+
+def get_data():
+    MAX_LEN = 75
+    bs = 16
+    data = pd.read_csv("ner_dataset.csv", encoding="latin1").fillna(method="ffill")
+    getter = SentenceGetter(data)
+    sentences = [" ".join([s[0] for s in sent]) for sent in getter.sentences]
+    labels = [[s[2] for s in sent] for sent in getter.sentences]
+    tags_vals = list(set(data["Tag"].values))
+    tag2idx = {t: i for i, t in enumerate(tags_vals)}
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
+    tokenized_texts = [tokenizer.tokenize(sent) for sent in sentences]
+    input_ids = pad_sequences([tokenizer.convert_tokens_to_ids(txt) for txt in tokenized_texts],
+                              maxlen=MAX_LEN, dtype="long", truncating="post", padding="post")
+    tags = pad_sequences([[tag2idx.get(l) for l in lab] for lab in labels],
+                         maxlen=MAX_LEN, value=tag2idx["O"], padding="post",
+                         dtype="long", truncating="post")
+    attention_masks = [[float(i > 0) for i in ii] for ii in input_ids]
+    tr_inputs, val_inputs, tr_tags, val_tags = train_test_split(input_ids, tags,
+                                                                random_state=2018, test_size=0.1)
+    tr_masks, val_masks, _, _ = train_test_split(attention_masks, input_ids,
+                                                 random_state=2018, test_size=0.1)
+    train_data = TensorDataset(tr_inputs, tr_masks, tr_tags)
+    train_sampler = RandomSampler(train_data)
+    train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=bs)
+
+    valid_data = TensorDataset(val_inputs, val_masks, val_tags)
+    valid_sampler = SequentialSampler(valid_data)
+    valid_dataloader = DataLoader(valid_data, sampler=valid_sampler, batch_size=bs)
+
+    return train_dataloader, valid_dataloader, tag2idx, tags_vals
+
+
+def finetuning(model, FULL_FINETUNING):
+    if FULL_FINETUNING:
+        param_optimizer = list(model.named_parameters())
+        no_decay = ['bias', 'gamma', 'beta']
+        optimizer_grouped_parameters = [
+            {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+             'weight_decay_rate': 0.01},
+            {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
+             'weight_decay_rate': 0.0}
+        ]
+    else:
+        param_optimizer = list(model.classifier.named_parameters())
+        optimizer_params = [{"params": [p for n, p in param_optimizer]}]
+
+    return optimizer_params
+
+
+def flat_accuracy(preds, labels):
+    pred_flat = np.argmax(preds, axis=2).flatten()
+    labels_flat = labels.flatten()
+    return np.sum(pred_flat == labels_flat) / len(labels_flat)
+
+
+def main():
+    train_dataloader, valid_dataloader, tag2idx, tags_vals = get_data()
+    model = BertForTokenClassification.from_pretrained("bert-base-uncased", num_labels=len(tag2idx))
+    model.cuda();
+    optimizer_params = finetuning(model, True)
+    optimizer = Adam(optimizer_params, lr=3e-5)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    epochs = 3
+    max_grad_norm = 1.0
+
+    for _ in trange(epochs, desc="Epoch"):
+        # TRAIN loop
+        model.train()
+        tr_loss = 0
+        nb_tr_examples, nb_tr_steps = 0, 0
+        for step, batch in enumerate(train_dataloader):
+            # add batch to gpu
+            batch = tuple(t.to(device) for t in batch)
+            b_input_ids, b_input_mask, b_labels = batch
+            # forward pass
+            loss = model(b_input_ids, token_type_ids=None,
+                         attention_mask=b_input_mask, labels=b_labels)
+            # backward pass
+            loss.backward()
+            # track train loss
+            tr_loss += loss.item()
+            nb_tr_examples += b_input_ids.size(0)
+            nb_tr_steps += 1
+            # gradient clipping
+            torch.nn.utils.clip_grad_norm_(parameters=model.parameters(), max_norm=max_grad_norm)
+            # update parameters
+            optimizer.step()
+            model.zero_grad()
+        # print train loss per epoch
+        print("Train loss: {}".format(tr_loss / nb_tr_steps))
+        # VALIDATION on validation set
+        model.eval()
+        eval_loss, eval_accuracy = 0, 0
+        nb_eval_steps, nb_eval_examples = 0, 0
+        predictions, true_labels = [], []
+        for batch in valid_dataloader:
+            batch = tuple(t.to(device) for t in batch)
+            b_input_ids, b_input_mask, b_labels = batch
+
+            with torch.no_grad():
+                tmp_eval_loss = model(b_input_ids, token_type_ids=None,
+                                      attention_mask=b_input_mask, labels=b_labels)
+                logits = model(b_input_ids, token_type_ids=None,
+                               attention_mask=b_input_mask)
+            logits = logits.detach().cpu().numpy()
+            label_ids = b_labels.to('cpu').numpy()
+            predictions.extend([list(p) for p in np.argmax(logits, axis=2)])
+            true_labels.append(label_ids)
+
+            tmp_eval_accuracy = flat_accuracy(logits, label_ids)
+
+            eval_loss += tmp_eval_loss.mean().item()
+            eval_accuracy += tmp_eval_accuracy
+
+            nb_eval_examples += b_input_ids.size(0)
+            nb_eval_steps += 1
+        eval_loss = eval_loss / nb_eval_steps
+        print("Validation loss: {}".format(eval_loss))
+        print("Validation Accuracy: {}".format(eval_accuracy / nb_eval_steps))
+        pred_tags = [tags_vals[p_i] for p in predictions for p_i in p]
+        valid_tags = [tags_vals[l_ii] for l in true_labels for l_i in l for l_ii in l_i]
+        print("F1-Score: {}".format(f1_score(pred_tags, valid_tags)))
+
+
+if __name__ == '__main__':
+    main()
